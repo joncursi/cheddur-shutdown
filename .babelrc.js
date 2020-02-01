79010ba98873c2cd6a965ec1f363fbdecb5cdb1b
Jon Cursi <joncursi@gmail.com>
2020-02-01 07:48:18 -0500

    build: Remove @material-ui/icons

--- .babelrc.js
+++ .babelrc.js
@@ -22,10 +22,6 @@ module.exports = api => {
           transform: '@material-ui/core/${member}',
           preventFullImport: true,
         },
-        '@material-ui/icons': {
-          transform: '@material-ui/icons/${member}',
-          preventFullImport: true,
-        },
         lodash: {
           transform: 'lodash/${member}',
           preventFullImport: true,
